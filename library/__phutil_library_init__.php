bf5a36f1928efa467407909e45c1611eadd2aee6
Cihad OGE <cihadoge@gmail.com>
2015-04-25 19:07:52 +0200

    push directly to github

---
+++ library/__phutil_library_init__.php
@@ -0,0 +1,3 @@
+<?php
+
+phutil_register_library('cbot-test-engine', __FILE__);
---
+++ library/src/CBOTTestEngine.php
@@ -0,0 +1,10 @@
+<?php
+
+final class CBOTTestEngine extends ArcanistUnitTestEngine {
+
+  public function run() {
+		assert(1==2);
+    return array();
+  }
+
+}
